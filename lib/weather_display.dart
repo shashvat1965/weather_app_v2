e5c77a572e5dfb7d35363188b9744c32d6cafed8
shashvat <shashvat1965@gmail.com>
2022-05-23 03:57:31 +0530

    idek

--- lib/weather_display.dart
+++ lib/weather_display.dart
@@ -1,45 +1,41 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+import 'package:weather_app_v2/Resources/images.dart';
 
 class WeatherDisplay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Expanded(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  children: const [Text("test text"), Text("test text")],
-                ),
-                const Icon(CupertinoIcons.search)
-              ],
-            ),
-            const Text(
-              "test temp",
-              style: const TextStyle(fontSize: 40),
-            ),
-            const Expanded(
-              child: Center(
-                child: Text("placeholder text"),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: const [Text("test text"), Text("test text")],
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  children: [Text("test text"), Text("test text")],
-                ),
-                Column(
-                  children: [Text("test text"), Text("test text")],
-                ),
-              ],
-            )
-          ],
-        ),
+              const Icon(CupertinoIcons.search)
+            ],
+          ),
+          const Text(
+            "test temp",
+            style: TextStyle(fontSize: 40),
+          ),
+          Center(
+            child: Image.network("https://picsum.photos/200/300")
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: const [Text("test text"), Text("test text")],
+              ),
+              Column(
+                children: [const Text("test text"), const Text("test text")],
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
